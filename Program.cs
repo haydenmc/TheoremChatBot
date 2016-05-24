a9767f069e38dcda2cc351473abf22c827a13cd4
Hayden McAfee <hayden@outlook.com>
2016-05-23 17:46:57 -0700

    Added a configuration file.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.WebSockets;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Theorem.Models;
 
@@ -9,6 +10,8 @@ namespace Theorem
 {
     public class Program
     {
+        public IConfigurationRoot Configuration { get; set; }
+        
         public static void Main(string[] args)
         {
             new Program().Start().Wait();
@@ -16,10 +19,15 @@ namespace Theorem
         
         public async Task Start()
         {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.default.json")
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
             using (var httpClient = new HttpClient())
             {
                 httpClient.BaseAddress = new Uri("https://slack.com/api/");
-                var startResult = await httpClient.GetAsync("rtm.start?token=TOKENHERE");
+                var startResult = await httpClient.GetAsync($"rtm.start?token={Configuration["Slack:ApiToken"]}");
                 var startStringResult = await startResult.Content.ReadAsStringAsync();
                 var startResponse = JsonConvert.DeserializeObject<StartResponseModel>(startStringResult);
                 if (startResult.IsSuccessStatusCode && startResponse.Ok)
